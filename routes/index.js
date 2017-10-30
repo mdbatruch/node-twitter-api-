283f6f67651627afe4e338c08d04df9f14ea9ce7
Mike Batruch <mdbatruch@Mikes-MacBook-Pro-3.local>
2017-10-30 09:27:18 -0400

    added date feature

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,31 @@ var client = new Twitter({
   access_token_secret: 'ACCESS TOKEN SECRET HERE'
 });
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 router.get('/', function(req, res, next) {
   // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-  client.get('statuses/user_timeline', { screen_name: 'mikebored89', count: 4 }, function(error, tweets, response) {
+  client.get('statuses/user_timeline', { screen_name: 'mroc_canada', count: 4 }, function(error, tweets, response) {
     if (!error) {
-      res.status(200).render('index', { title: 'Express', tweets: tweets });
+        // if a tweet doesn't have media, filter it out
+        // only display images for tweets that have a media property
+          console.log(tweets);
+        var tweets = tweets.map(function(tweet) {
+           tweet.created_at = formatDate(tweet.created_at);
+            return tweet;
+        });
+//        res.status(200).json(tweets);
+              res.status(200).render('index', { title: 'Express', tweets: tweets, exclude_replies: false});
     }
     else {
       res.status(500).json({ error: error });
